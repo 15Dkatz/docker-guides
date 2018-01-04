1cbb6275766a6d237f22ec27658689905e2f3db0
David Katz <15Dkatz@shcp.edu>
2018-01-04 13:02:09 -0800

    Add sitetest.sh

---
+++ serve/sitetest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if curl http://localhost:5000 | grep site/; then
+  echo "Site exists.\nSite test passes.\n"
+  exit 0
+else
+  echo "Site does not exist.\nSite test fails."
+  exit 1
+fi
